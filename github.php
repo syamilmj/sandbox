f838eaae9de30e87a6d85e7042a35853c9f51a35
sy4mil <sy4mil@gmail.com>
2013-03-25 19:41:02 +0800

    update

---
+++ github.php
@@ -0,0 +1,13 @@
+<?php
+$github_ips = array('207.97.227.253', '50.57.128.197', '108.171.174.178');
+ 
+if(in_array($_SERVER['REMOTE_ADDR'], $github_ips)) {
+    $dir = '/var/www/path/to/your/git/root';
+    exec("cd $dir && git pull");
+    echo 'Done.';
+}
+else {
+    header('HTTP/1.1 404 Not Found');
+    echo '404 Not Found.';
+    exit;
+}
