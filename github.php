744c5110e8425f80191e6833d7fb66a090be3d93
sy4mil <sy4mil@gmail.com>
2013-03-26 04:42:12 +0800

    update public github ips

--- github.php
+++ github.php
@@ -1,1 +1,12 @@
-<?php `git pull`;
+<?php
+$github_ips = array('207.97.227.253, 50.57.128.197, 108.171.174.178, 50.57.231.61, 204.232.175.64, 192.30.252.0');
+
+if(in_array($_SERVER['REMOTE_ADDR'], $github_ips)) {
+    `git pull`;
+    echo 'Done.';
+}
+else {
+    header('HTTP/1.1 404 Not Found');
+    echo '404 Not Found.';
+    exit;
+}
