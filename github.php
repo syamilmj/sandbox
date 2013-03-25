955e547f452855b2ee59639758c863feb841d1d8
sy4mil <sy4mil@gmail.com>
2013-03-26 04:10:07 +0800

    try

--- github.php
+++ github.php
@@ -2,8 +2,8 @@
 $github_ips = array('207.97.227.253', '50.57.128.197', '108.171.174.178');
  
 if(in_array($_SERVER['REMOTE_ADDR'], $github_ips)) {
-    $dir = '/domains/sandbox.aquagraphite.com/html/sandbox';
-    exec("cd $dir && git pull");
+    $dir = '/domains/sandbox.aquagraphite.com/html';
+    exec("cd $dir && git pull git@github.com:sy4mil/sandbox.git");
     echo 'Done.';
 }
 else {
