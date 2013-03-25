81fdc88badf8688934fbd7408f629ad814ec43aa
sy4mil <sy4mil@gmail.com>
2013-03-26 04:17:33 +0800

    try 2

--- github.php
+++ github.php
@@ -1,2 +1,13 @@
 <?php
-git pull git@github.com:sy4mil/sandbox.git
+$github_ips = array('207.97.227.253', '50.57.128.197', '108.171.174.178');
+ 
+if(in_array($_SERVER['REMOTE_ADDR'], $github_ips)) {
+    $dir = getcwd();
+    exec("cd $dir && git pull git@github.com:sy4mil/sandbox.git");
+    echo 'Done.';
+}
+else {
+    header('HTTP/1.1 404 Not Found');
+    echo '404 Not Found.';
+    exit;
+}
