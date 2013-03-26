1b5908133b48b8a80756dcd321e8901d9e7949f3
Syamil MJ <sy4mil@gmail.com>
2013-03-27 01:14:35 +0800

    prettifaaaaaaaaaiiiiiii

--- github.php
+++ github.php
@@ -4,8 +4,7 @@ $github_ips = array('207.97.227.253, 50.57.128.197, 108.171.174.178, 50.57.231.6
 if(in_array($_SERVER['REMOTE_ADDR'], $github_ips)) {
     `git pull`;
     echo 'Done.';
-}
-else {
+} else {
     header('HTTP/1.1 404 Not Found');
     echo '404 Not Found.';
     exit;
