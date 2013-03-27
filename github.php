3174f3db303fe19d4696aa2481dda5736112011a
sy4mil <sy4mil@gmail.com>
2013-03-27 17:39:04 +0800

    my ip

--- github.php
+++ github.php
@@ -1,6 +1,6 @@
 <?php
 $github_ips = array('207.97.227.253, 50.57.128.197, 108.171.174.178, 50.57.231.61, 204.232.175.64, 192.30.252.0');
-
+echo $_SERVER['REMOTE_ADDR'];
 if(in_array($_SERVER['REMOTE_ADDR'], $github_ips)) {
     `git pull`;
     echo 'Done.';
